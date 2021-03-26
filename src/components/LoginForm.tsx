6bfb0fc9470f247b65458f5210be085baec3de77
Sopanat <sopanus2014@gmail.com>
2021-03-26 17:47:55 +0700

    feat: rendering login when service working.

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -25,75 +25,74 @@ const LoginForm = () => {
   const [password, setPassword] = useState("");
   return (
     <Flex minHeight="100vh" width="full" align="center" justifyContent="center">
-      <Box
-        borderWidth={1}
-        px={4}
-        width="full"
-        maxWidth="500px"
-        borderRadius={4}
-        textAlign="center"
-        boxShadow="lg"
-      >
-        <Box p={4}>
-          <Box my={8} textAlign="left">
-            {!loading && !error && data ? (
-              <Text>สถานะระบบ : {data.getServiceStatus}</Text>
-            ) : (
-              <Text>กำลังโหลด ....</Text>
-            )}
-            <Text
-              fontSize="2xl"
-              fontWeight="bold"
-              color="#4A5568"
-              alignItems="center"
-              marginBottom={10}
-            >
-              เข้าสู่ระบบจัดการเอกสาร
-            </Text>
-            <form>
-              <FormControl isRequired>
-                <FormLabel>ชื่อผู้ใช้ </FormLabel>
-                <Input
-                  type="text"
-                  placeholder="กรอกชื่อผู้ใช้งาน"
-                  onChange={(event) => setUsername(event.currentTarget.value)}
-                />
-              </FormControl>
-
-              <FormControl mt={4} isRequired>
-                <FormLabel>รหัสผ่าน</FormLabel>
-                <Input
-                  type="password"
-                  placeholder="กรอกรหัสผ่าน"
-                  onChange={(event) => setPassword(event.currentTarget.value)}
-                />
-              </FormControl>
-
-              <Stack isInline justifyContent="space-between" mt={4}>
-                <Box>
-                  <Checkbox>จดจำการเข้าสู่ระบบ</Checkbox>
-                </Box>
-                <Box>
-                  <Link color={`${VARIANT_COLOR}.500`} href="login">
-                    ลืมรหัสผ่าน?
-                  </Link>
-                </Box>
-              </Stack>
-
-              <Button
-                variantColor={VARIANT_COLOR}
-                width="full"
-                mt={4}
-                colorScheme="blue"
-                type="submit"
-                onClick={() => testlalert()}
+      {!loading && !error && data ? (
+        <Box
+          borderWidth={1}
+          px={4}
+          width="full"
+          maxWidth="500px"
+          borderRadius={4}
+          textAlign="center"
+          boxShadow="lg"
+        >
+          <Box p={4}>
+            <Box my={8} textAlign="left">
+              <Text
+                fontSize="2xl"
+                fontWeight="bold"
+                color="#4A5568"
+                alignItems="center"
+                marginBottom={10}
               >
-                เข้าสู่ระบบ
-              </Button>
-            </form>
+                เข้าสู่ระบบจัดการเอกสาร
+              </Text>
+              <form>
+                <FormControl isRequired>
+                  <FormLabel>ชื่อผู้ใช้ </FormLabel>
+                  <Input
+                    type="text"
+                    placeholder="กรอกชื่อผู้ใช้งาน"
+                    onChange={(event) => setUsername(event.currentTarget.value)}
+                  />
+                </FormControl>
+
+                <FormControl mt={4} isRequired>
+                  <FormLabel>รหัสผ่าน</FormLabel>
+                  <Input
+                    type="password"
+                    placeholder="กรอกรหัสผ่าน"
+                    onChange={(event) => setPassword(event.currentTarget.value)}
+                  />
+                </FormControl>
+
+                <Stack isInline justifyContent="space-between" mt={4}>
+                  <Box>
+                    <Checkbox>จดจำการเข้าสู่ระบบ</Checkbox>
+                  </Box>
+                  <Box>
+                    <Link color={`${VARIANT_COLOR}.500`} href="login">
+                      ลืมรหัสผ่าน?
+                    </Link>
+                  </Box>
+                </Stack>
+
+                <Button
+                  variantColor={VARIANT_COLOR}
+                  width="full"
+                  mt={4}
+                  colorScheme="blue"
+                  type="submit"
+                  onClick={() => testlalert()}
+                >
+                  เข้าสู่ระบบ
+                </Button>
+              </form>
+            </Box>
           </Box>
         </Box>
-      </Box>
+      ) : (
+        <Text>Connecting to the server...</Text>
+      )}
     </Flex>
   );
 };
