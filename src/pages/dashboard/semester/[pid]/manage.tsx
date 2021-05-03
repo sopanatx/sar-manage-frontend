1a192c5afa96b557752d098cf3f77715e350cd5b
Sopanat <sopanus2014@gmail.com>
2021-05-03 13:04:03 +0700

    removed lorem imsum

--- src/pages/dashboard/semester/[pid]/manage.tsx
+++ src/pages/dashboard/semester/[pid]/manage.tsx
@@ -82,13 +82,7 @@ const ManageDocument = () => {
                             <AccordionIcon />
                           </AccordionButton>
                         </h2>
-                        <AccordionPanel pb={4}>
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et
-                          dolore magna aliqua. Ut enim ad minim veniam, quis
-                          nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea commodo consequat.
-                        </AccordionPanel>
+                        <AccordionPanel pb={4}></AccordionPanel>
                       </AccordionItem>
                     </>
                   ))}
