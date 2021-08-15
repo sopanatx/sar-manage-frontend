6cd34680efdf497b2510203e4a81fc4db8bc6dd4
Sopanat Suradach <sopanus2014@gmail.com>
2021-08-15 21:59:36 +0700

    feat: Added AdminUpdateSubCategoryMutation Schema

--- src/mutation/AdminUpdateSubCategory.ts
+++ src/mutation/AdminUpdateSubCategory.ts
@@ -1,4 +1,10 @@
 import { gql } from "@apollo/client";
 
-const ADMIN_UPDATE_SUBCATEGORY_MUTATION = gql``;
+const ADMIN_UPDATE_SUBCATEGORY_MUTATION = gql`
+  mutation AdminUpdateSubCategoryMutation(
+    $adminUpdateSubCategory: AdminUpdateSubCategoryDto!
+  ) {
+    AdminUpdateSubCategory(UpdateSubCategoryDto: $adminUpdateSubCategory)
+  }
+`;
 export default ADMIN_UPDATE_SUBCATEGORY_MUTATION;
